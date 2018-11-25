6fb2968946fd8529e2ea50736e669d04e34476ce
Alican Akkus <alican.akkus94@gmail.com>
2018-11-25 15:22:49 +0300

    starting project

---
+++ src/main/java/com/aakkus/dockercomposeinitializr/DockerComposeInitializrApplication.java
@@ -0,0 +1,12 @@
+package com.aakkus.dockercomposeinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposeInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerComposeInitializrApplication.class, args);
+    }
+}
